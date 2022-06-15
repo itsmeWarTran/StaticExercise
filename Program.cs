0d69db2a6a704e790bc0df3919b138dab3aa3fcb
Warren Tran <warrentran2015@hotmail.com>
2022-06-15 14:30:17 -0400

    descriptive message

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cel = TempConverter.fahToCel(32);
+            var fah = TempConverter.celToFah(32);
+
+            Console.WriteLine("The conversion of 32 degrees celcius to fahrenheit is " + cel);
+            Console.WriteLine("The conversion of 32 degrees fahrenheit to celcius is " + fah);
+        }
+
+        
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        public static double fahToCel(double temp)
+        {
+            double Celsius = (temp - 32) / 1.8;
+            return Celsius;
+        }
+
+        public static double celToFah(double temp)
+        {
+            double Fahrenheit = (temp * 9) / 5 + 32;
+            return Fahrenheit;
+        }
+    }
+}
